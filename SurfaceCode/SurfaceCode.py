11897e8e70e9bf647c728632c3545c98ad88e255
ZhenghaoDing <zding@iwu.edu>
2020-06-28 23:25:21 -0400

    debugged error syndrome measurement: measuring the stabilizers correctly

--- SurfaceCode/SurfaceCode.py
+++ SurfaceCode/SurfaceCode.py
@@ -42,6 +42,13 @@ class SurfaceCode():
         self.ancilla_bits = []
         self.code_patches=[]
         self.side_code_patches=[]
+        
+        self.x_block_patches=[]
+        self.z_block_patches=[]
+        self.x_side_patches=[]
+        self.z_side_patches=[]
+        
+        
         self.data_bit = ClassicalRegister(self.d**2,'code_bit')
 
         self.circuit = {}
@@ -51,12 +58,12 @@ class SurfaceCode():
         self._preparation()
         
         for _ in range(T-1):
-            self.x_syndrome_measurement(reset=True,barrier=True)
-            self.z_syndrome_measurement(reset=True,barrier=True)
-        
+            self.stabilizer_circuit(reset=True)
+            self.measurement()
+
         if T!=0:
-            self.x_syndrome_measurement(reset=False)
-            self.z_syndrome_measurement(reset=False)
+            self.stabilizer_circuit(reset=False)
+            self.measurement()
             
     def data_x(self, logs=('0', '1'), barrier=False):
         """
@@ -96,14 +103,18 @@ class SurfaceCode():
         encode a 1.
         """
         self.data_x(['1'])
-        self.ancilla_H()
         
     def code_patches_generation(self):
+        """
+        Generating the code patches for stabilizer circuit construction;
+    
+        """
         code_patches=[]
         code_patch_index = 0
         line_index = 0
         ancilla_index = 0
         
+        # set up the ancilla_bits;
         self.ancilla_bits.append(ClassicalRegister((self.d**2-1),'round_'+str(self.T)+'_link_bit'))
         
         for j in range(self.d-1):
@@ -120,7 +131,38 @@ class SurfaceCode():
                 ancilla_index += 1
             line_index += self.d
         
-                        
+        x_block_patches=[]
+        z_block_patches=[]
+        
+        if not (self.d%2):
+            for i in range(len(code_patches)):
+                if i%2:
+                    x_block_patches.append(code_patches[i])
+                else:
+                    z_block_patches.append(code_patches[i])
+        else:
+            squared=[]
+            for m in range(len(code_patches)//(self.d-1)):
+                a=[]
+                for k in range(self.d-1):
+                    a.append(code_patches[k+(self.d-1)*m])
+                squared.append(a)
+        
+            for m in range(self.d-1):
+                if m%2:
+                    for k in range(self.d-1):
+                        if not (k%2):
+                            x_block_patches.append(squared[m][k])
+                        else:
+                            z_block_patches.append(squared[m][k])
+                else:
+                    for k in range(self.d-1):
+                        if not (k%2):
+                            z_block_patches.append(squared[m][k])
+                        else:
+                            x_block_patches.append(squared[m][k])
+                
+        
         # create the side code batch index list;
         qubit_index_arr = [i for i in range(self.d**2)]
         side_patch_index = qubit_index_arr[:self.d]
@@ -146,7 +188,6 @@ class SurfaceCode():
         b.sort(reverse=True)
         side_patch_index=side_patch_index+b
         
-        
         # create the side patches;
         side_code_patches=[]
         side_code_patch_index = 0
@@ -161,81 +202,210 @@ class SurfaceCode():
             ancilla_index +=1
             side_code_patches.append(code_patch)
         
+        top_num = self.d//2
+        spacing = (len(side_code_patches) - 2*top_num)//2
+        x_side_patches = side_code_patches[:top_num]+\
+                            side_code_patches[len(side_code_patches)-spacing-top_num:len(side_code_patches)-spacing]
+        
+        z_side_patches = side_code_patches[top_num:top_num+spacing]+\
+                            side_code_patches[top_num*2+spacing:]
+
+        
         self.code_patches=code_patches
         self.side_code_patches=side_code_patches
+        self.x_block_patches=x_block_patches
+        self.x_side_patches=x_side_patches
+        self.z_block_patches=z_block_patches
+        self.z_side_patches=z_side_patches
         
         
-    def x_syndrome_measurement(self, reset=True, barrier=False):
+    def stabilizer_circuit(self, reset=True):
+        """
+        Generating the circuits for the stabilizers of the surface code with NO measurements done;
         
+        """
         self.code_patches_generation()
-            
-        x_sequence = [0,1,2,3]
         
+        # building the circuit;
+        
+        # initialize all the ancilla qubits for the x blocks;
         for log in ['0','1']:
-            self.circuit[log].add_register(self.ancilla_bits[-1])
-            for j in range(len(x_sequence)):
-                for i in range(len(self.code_patches)):
-                    ancilla = self.code_patches[i][i][len(self.code_patches[i][i])-1][len(self.code_patches[i][i])-1]
-                    if i%2:
-                        qubit = self.code_patches[i][i][x_sequence[j]][x_sequence[j]]
-                        self.circuit[log].cx(ancilla,qubit)
-    
-            for i in range(len(self.side_code_patches)):
-                index_1 = self.side_code_patches[i][i][0][0].index
-                index_2 = self.side_code_patches[i][i][1][1].index
-                if not ((index_1<self.d or index_1>(self.d**2-self.d)) and (index_2<self.d or index_2>(self.d**2-self.d-1))):
-                    qubit_1 = self.side_code_patches[i][i][0][0]
-                    qubit_2 = self.side_code_patches[i][i][1][1]
-                    ancilla = self.side_code_patches[i][i][2][2]
-                    self.circuit[log].cx(ancilla,qubit_1)
-                    self.circuit[log].cx(ancilla,qubit_2)
+            for i in range(len(self.x_block_patches)):
+                key = [*self.x_block_patches[i]][0]
+                ancilla = self.x_block_patches[i][key][len(self.x_block_patches[i][key])-1][len(self.x_block_patches[i][key])-1]
+                self.circuit[log].h(ancilla)
+            for i in range(len(self.x_side_patches)):
+                key = [*self.x_side_patches[i]][0]
+                ancilla = self.x_side_patches[i][key][len(self.x_side_patches[i][key])-1][len(self.x_side_patches[i][key])-1]
+                self.circuit[log].h(ancilla)
+                
+        
+        # the first cnot gate;
+        
+        # for all the block_patches;
+        for log in ['0','1']:
+            
             self.circuit[log].barrier()
+            # for the x_blocks;
+            for i in range(len(self.x_block_patches)):
+                key = [*self.x_block_patches[i]][0]
+                qubit = self.x_block_patches[i][key][0][0]
+                ancilla = self.x_block_patches[i][key][len(self.x_block_patches[i][key])-1][len(self.x_block_patches[i][key])-1]
+                self.circuit[log].cx(ancilla,qubit)
+        
+            
+            # for the bottom x_sides;
+            for i in range(len(self.x_side_patches[(self.d-1)//2:])):
+                code_patch = self.x_side_patches[(self.d-1)//2:][i]
+                key = [*code_patch][0]
+                qubit = code_patch[key][1][1]
+                ancilla = code_patch[key][len(code_patch[key])-1][len(code_patch[key])-1]
+                self.circuit[log].cx(ancilla,qubit)
+            
+            # for the z_blocks;
+            for i in range(len(self.z_block_patches)):
+                key = [*self.z_block_patches[i]][0]
+                qubit = self.z_block_patches[i][key][0][0]
+                ancilla = self.z_block_patches[i][key][len(self.z_block_patches[i][key])-1][len(self.z_block_patches[i][key])-1]
+                self.circuit[log].cz(ancilla,qubit)
+            
+            # for the right_z_sides;
+            for i in range(len(self.z_side_patches[:(self.d-1)//2])):
+                code_patch = self.z_side_patches[:(self.d-1)//2][i]
+                key = [*code_patch][0]
+                qubit = code_patch[key][0][0]
+                ancilla = code_patch[key][len(code_patch[key])-1][len(code_patch[key])-1]
+                self.circuit[log].cz(ancilla,qubit)
+                
+            #####################################
+            #####################################
+            
+            self.circuit[log].barrier()
+            # for the x_blocks;
+            for i in range(len(self.x_block_patches)):
+                key = [*self.x_block_patches[i]][0]
+                qubit = self.x_block_patches[i][key][2][2]
+                ancilla = self.x_block_patches[i][key][len(self.x_block_patches[i][key])-1][len(self.x_block_patches[i][key])-1]
+                self.circuit[log].cx(ancilla,qubit)
+        
+            
+            # for the top x_sides;
+            for i in range(len(self.x_side_patches[:(self.d-1)//2])):
+                code_patch = self.x_side_patches[:(self.d-1)//2][i]
+                key = [*code_patch][0]
+                qubit = code_patch[key][0][0]
+                ancilla = code_patch[key][len(code_patch[key])-1][len(code_patch[key])-1]
+                self.circuit[log].cx(ancilla,qubit)
+            
+            # for the z_blocks;
+            for i in range(len(self.z_block_patches)):
+                key = [*self.z_block_patches[i]][0]
+                qubit = self.z_block_patches[i][key][1][1]
+                ancilla = self.z_block_patches[i][key][len(self.z_block_patches[i][key])-1][len(self.z_block_patches[i][key])-1]
+                self.circuit[log].cz(ancilla,qubit)
+            
+            # for the left_z_sides;
+            for i in range(len(self.z_side_patches[(self.d-1)//2:])):
+                code_patch = self.z_side_patches[(self.d-1)//2:][i]
+                key = [*code_patch][0]
+                qubit = code_patch[key][1][1]
+                ancilla = code_patch[key][len(code_patch[key])-1][len(code_patch[key])-1]
+                self.circuit[log].cz(ancilla,qubit)
+        
+            #####################################
+            #####################################
+            
+            self.circuit[log].barrier()
+            # for the x_blocks;
+            for i in range(len(self.x_block_patches)):
+                key = [*self.x_block_patches[i]][0]
+                qubit = self.x_block_patches[i][key][1][1]
+                ancilla = self.x_block_patches[i][key][len(self.x_block_patches[i][key])-1][len(self.x_block_patches[i][key])-1]
+                self.circuit[log].cx(ancilla,qubit)
+        
+            
+            # for the bottom x_sides;
+            for i in range(len(self.x_side_patches[(self.d-1)//2:])):
+                code_patch = self.x_side_patches[(self.d-1)//2:][i]
+                key = [*code_patch][0]
+                qubit = code_patch[key][0][0]
+                ancilla = code_patch[key][len(code_patch[key])-1][len(code_patch[key])-1]
+                self.circuit[log].cx(ancilla,qubit)
+            
+            # for the z_blocks;
+            for i in range(len(self.z_block_patches)):
+                key = [*self.z_block_patches[i]][0]
+                qubit = self.z_block_patches[i][key][2][2]
+                ancilla = self.z_block_patches[i][key][len(self.z_block_patches[i][key])-1][len(self.z_block_patches[i][key])-1]
+                self.circuit[log].cz(ancilla,qubit)
+            
+            # for the right_z_sides;
+            for i in range(len(self.z_side_patches[:(self.d-1)//2])):
+                code_patch = self.z_side_patches[:(self.d-1)//2][i]
+                key = [*code_patch][0]
+                qubit = code_patch[key][1][1]
+                ancilla = code_patch[key][len(code_patch[key])-1][len(code_patch[key])-1]
+                self.circuit[log].cz(ancilla,qubit)
+            
+            #####################################
+            #####################################
+            
+            self.circuit[log].barrier()
+            # for the x_blocks;
+            for i in range(len(self.x_block_patches)):
+                key = [*self.x_block_patches[i]][0]
+                qubit = self.x_block_patches[i][key][3][3]
+                ancilla = self.x_block_patches[i][key][len(self.x_block_patches[i][key])-1][len(self.x_block_patches[i][key])-1]
+                self.circuit[log].cx(ancilla,qubit)
+        
+            
+            # for the top x_sides;
+            for i in range(len(self.x_side_patches[:(self.d-1)//2])):
+                code_patch = self.x_side_patches[:(self.d-1)//2][i]
+                key = [*code_patch][0]
+                qubit = code_patch[key][1][1]
+                ancilla = code_patch[key][len(code_patch[key])-1][len(code_patch[key])-1]
+                self.circuit[log].cx(ancilla,qubit)
+            
+            # for the z_blocks;
+            for i in range(len(self.z_block_patches)):
+                key = [*self.z_block_patches[i]][0]
+                qubit = self.z_block_patches[i][key][3][3]
+                ancilla = self.z_block_patches[i][key][len(self.z_block_patches[i][key])-1][len(self.z_block_patches[i][key])-1]
+                self.circuit[log].cz(ancilla,qubit)
+            
+            # for the left_z_sides;
+            for i in range(len(self.z_side_patches[(self.d-1)//2:])):
+                code_patch = self.z_side_patches[(self.d-1)//2:][i]
+                key = [*code_patch][0]
+                qubit = code_patch[key][0][0]
+                ancilla = code_patch[key][len(code_patch[key])-1][len(code_patch[key])-1]
+                self.circuit[log].cz(ancilla,qubit)
+                
             for i in range(self.d**2-1):
-                self.circuit[log].measure(self.ancilla_qubit[i],self.ancilla_bits[self.T][i])
                 if reset:
                     self.circuit[log].reset(self.ancilla_qubit[i])
             self.circuit[log].barrier()
                 
         self.T +=1
-            
-    def z_syndrome_measurement(self, reset=True, barrier=False):
         
-        self.code_patches_generation()
+    def get_circuit_list(self):
+        """
+        Returns:
+            circuit_list: self.circuit as a list, with
+            circuit_list[0] = circuit['0']
+            circuit_list[1] = circuit['1']
+        """
+        circuit_list = [self.circuit[log] for log in ['0', '1']]
+        return circuit_list
+    
             
-        z_sequence = [0,2,1,3]
-        
+    def measurement(self):
+        """
+        Measure the ancilla qubits of the circuit;
+        """
+        self.ancilla_bits.append(ClassicalRegister((self.d**2-1),'round_'+str(self.T)+'_link_bit'))
         for log in ['0','1']:
             self.circuit[log].add_register(self.ancilla_bits[-1])
-            for j in range(len(z_sequence)):
-                for i in range(len(self.code_patches)):
-                    ancilla = self.code_patches[i][i][len(self.code_patches[i][i])-1][len(self.code_patches[i][i])-1]
-                    if not (i%2):
-                        qubit = self.code_patches[i][i][z_sequence[j]][z_sequence[j]]
-                        self.circuit[log].cz(ancilla,qubit)
-    
-            for i in range(len(self.side_code_patches)):
-                index_1 = self.side_code_patches[i][i][0][0].index
-                index_2 = self.side_code_patches[i][i][1][1].index
-                if (index_1<self.d or index_1>(self.d**2-self.d)) and (index_2<self.d or index_2>(self.d**2-self.d-1)):
-                    qubit_1 = self.side_code_patches[i][i][0][0]
-                    qubit_2 = self.side_code_patches[i][i][1][1]
-                    ancilla = self.side_code_patches[i][i][2][2]
-                    self.circuit[log].cz(ancilla,qubit_1)
-                    self.circuit[log].cz(ancilla,qubit_2)
-            self.circuit[log].barrier()
             for i in range(self.d**2-1):
                 self.circuit[log].measure(self.ancilla_qubit[i],self.ancilla_bits[self.T][i])
-                if reset:
-                    self.circuit[log].reset(self.ancilla_qubit[i])
-            self.circuit[log].barrier()
-                
-        self.T +=1
-        
-    def readout(self):
-        """
-        Readout of all code qubits, which corresponds to a logical measurement
-        as well as allowing for a measurement of the syndrome to be inferred.
-        """
-        for log in ['0', '1']:
-            self.circuit[log].add_register(self.data_bit)
-            self.circuit[log].measure(self.data_qubit, self.data_bit)
