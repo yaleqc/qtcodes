8be8f2a9270bd7890e68fde1e701d0443e5d57d0
Shraddha SINGH <37458529+shraggy@users.noreply.github.com>
2020-07-01 12:51:57 -0400

    Update circuits.py

--- surface_code/circuits.py
+++ surface_code/circuits.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-# Following has been inspired from the code (qiskit.ignis.verification.topological_codes.circuits.py) that is licensed under the Apache License, Version 2.0. You may
-# obtain a copy of this license in the LICENSE.txt file in the root directory
-# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
 
 """Generates circuits for quantum error correction with surface code patches."""
 
